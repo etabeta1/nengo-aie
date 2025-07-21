7f0f0948253addad0f2306d7941ac32c923d9f7b
Andrea Oggioni (aupcloud) <24810621+etabeta1@users.noreply.github.com>
2025-07-21 12:04:33 +0000

    chore: add code used for the benchmarks during the AMD presentation

--- nengo_aie/aie_kernels/ElementwiseInc/MlirBuilder.py
+++ nengo_aie/aie_kernels/ElementwiseInc/MlirBuilder.py
@@ -14,36 +14,37 @@ class MlirBuilder(MlirBuilderBase.MlirBuilder):
 
     def build(self, device, size):
         value_type = np.half
-        param_type = np.ndarray[(1, ), np.dtype[np.int32]]
-        input_type = np.ndarray[(3 * size, ), np.dtype[value_type]]
-        output_type = np.ndarray[(3 * size, ), np.dtype[value_type]]
+
+        entire_input_type = np.ndarray[(3 * size, ), np.dtype[value_type]]
+        entire_output_type = np.ndarray[(size, ), np.dtype[value_type]]
+        
+        input_type = np.ndarray[(96, ), np.dtype[value_type]]
+        output_type = np.ndarray[(32, ), np.dtype[value_type]]
     
         kernel_fn = Kernel(
             "elementwise_inc",
             "kernel.o",
-            [param_type, input_type, output_type]
+            [input_type, output_type]
         )
     
-        of_in_param = ObjectFifo(param_type, name="param_input_fifo")
-        of_in_data = ObjectFifo(input_type, name="data_input_fifo")
+        # of_in_param = ObjectFifo(param_type, name="param_input_fifo")
+        of_in_data = ObjectFifo(input_type, name="input_fifo")
         of_out = ObjectFifo(output_type, name="output_fifo")
     
-        def core_fn(of_param, of_data, of_out, kernel):
-            p = of_param.acquire(1)
-            o = of_out.acquire(1)
-            i = of_data.acquire(1)
-            kernel(p, i, o)
-            of_data.release(1)
-            of_out.release(1)
-            of_param.release(1)
+        def core_fn(of_data, of_out, kernel):
+            for _ in range_(0xFFFFFFFF):
+                o = of_out.acquire(1)
+                i = of_data.acquire(1)
+                kernel(i, o)
+                of_data.release(1)
+                of_out.release(1)
     
-        worker = Worker(core_fn, [of_in_param.cons(), of_in_data.cons(), of_out.prod(), kernel_fn])
+        worker = Worker(core_fn, [of_in_data.cons(), of_out.prod(), kernel_fn])
     
         rt = Runtime()
     
-        with rt.sequence(param_type, input_type, output_type) as (p, i, o):
+        with rt.sequence(entire_input_type, entire_output_type) as (i, o):
             rt.start(worker)
-            rt.fill(of_in_param.prod(), p)
             rt.fill(of_in_data.prod(), i)
             rt.drain(of_out.cons(), o, wait=True)
     
