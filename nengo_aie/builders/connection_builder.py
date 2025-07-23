12c2d992f0ea99b18f80618c2e44acdd09ab0e0c
andrea <24810621+etabeta1@users.noreply.github.com>
2025-07-23 11:26:47 +0200

    feat: add dummy builder implementation that works as the default one

--- nengo_aie/builders/connection_builder.py
+++ nengo_aie/builders/connection_builder.py
@@ -1,37 +1,42 @@
 import nengo
+
+import nengo.builder
+import nengo.connection
+import nengo.ensemble
+import nengo.neurons
+import nengo.transforms
+import numpy as np
+
+from nengo.builder.node import SimPyFunc as AIESimPyFunc
+from nengo.builder.operator import Copy as AIECopy, ElementwiseInc as AIEElementwiseInc, Reset as AIEReset
+from nengo.connection import Connection as AIEConnection
+from nengo.ensemble import Ensemble as AIEEnsemble, Neurons as AIENeurons
+from nengo.node import Node as AIENode
+from nengo.rc import rc
+from nengo.solvers import NoSolver as AIENoSolver, Solver as AIESolver
+from nengo.transforms import Dense as AIEDense, NoTransform as AIENoTransform
+
 from ..builder import AIEBuilder
 
-import numpy as np
-from nengo.builder.ensemble import gen_eval_points, get_activities
-from nengo.builder.node import SimPyFunc
-from nengo.builder.operator import Copy, ElementwiseInc, Reset
-from nengo.builder.signal import Signal
-from nengo.connection import Connection
-from nengo.ensemble import Ensemble, Neurons
-from nengo.exceptions import BuildError
-from nengo.neurons import Direct
-from nengo.node import Node
-from nengo.rc import rc
-from nengo.solvers import NoSolver, Solver
-from nengo.transforms import Dense, NoTransform
-from nengo.utils.numpy import is_integer, is_iterable
+# TODO: change to AIEConnection (the one from nengo-aie)
 
-@AIEBuilder.register(nengo.connection.Connection)
-def build_connection(model, conn):
+
+@AIEBuilder.register(AIEConnection)
+def build_aie_connection(model: nengo.builder.Model, conn):
     rng = np.random.RandomState(model.seeds[conn])
-    
+
     def get_prepost_signal(is_pre):
         target = conn.pre_obj if is_pre else conn.post_obj
         key = "out" if is_pre else "in"
 
         if target not in model.sig:
-            raise BuildError(
+            raise nengo.exceptions.BuildError(
                 f"Building {conn}: the '{'pre' if is_pre else 'post'}' object {target} "
                 "is not in the model, or has a size of zero."
             )
         signal = model.sig[target].get(key, None)
         if signal is None or signal.size == 0:
-            raise BuildError(
+            raise nengo.exceptions.BuildError(
                 f"Building {conn}: the '{'pre' if is_pre else 'post'}' object {target} "
                 f"has a '{key}' size of zero."
             )
@@ -47,105 +52,74 @@ def build_connection(model, conn):
     solver_info = None
     post_slice = conn.post_slice
 
-    # Figure out the signal going across this connection
     in_signal = model.sig[conn]["in"]
-    if isinstance(conn.pre_obj, Node) or (
-        isinstance(conn.pre_obj, Ensemble)
-        and isinstance(conn.pre_obj.neuron_type, Direct)
-    ):
-        # Node or Decoded connection in directmode
-        sliced_in = nengo.builder.connection.slice_signal(model, in_signal, conn.pre_slice)
+
+    if isinstance(conn.pre_obj, nengo.Node) or (isinstance(conn.pre_obj, nengo.Ensemble) and isinstance(conn.pre_obj.neuron_type, nengo.Direct)):
+        sliced_in = nengo.builder.connection.slice_signal(
+            model, in_signal, conn.pre_slice)
         if conn.function is None:
             in_signal = sliced_in
         elif isinstance(conn.function, np.ndarray):
-            raise BuildError("Cannot use function points in direct connection")
+            raise nengo.exceptions.BuildError(
+                "Cannot use function points in direct connection")
         else:
-            in_signal = Signal(shape=conn.size_mid, name=f"{conn}.func")
-            model.add_op(SimPyFunc(in_signal, conn.function, None, sliced_in))
-    elif isinstance(conn.pre_obj, Ensemble):  # Normal decoded connection
-        eval_points, decoders, solver_info = model.build(conn.solver, conn, rng)
-        if isinstance(conn.post_obj, Ensemble) and conn.solver.weights:
+            in_signal = nengo.builder.Signal(shape=conn.size_mid, name=f"{conn}.func")
+            model.add_op(AIESimPyFunc(in_signal, conn.function, None, sliced_in))
+    elif isinstance(conn.pre_obj, nengo.Ensemble):
+        eval_points, decoders, solver_info = model.build(
+            conn.solver, conn, rng)  # type: ignore
+        if isinstance(conn.post_obj, nengo.Ensemble) and conn.solver.weights:
             model.sig[conn]["out"] = model.sig[conn.post_obj.neurons]["in"]
-
             encoders = model.params[conn.post_obj].scaled_encoders.T
             encoders = encoders[conn.post_slice]
 
-            # post slice already applied to encoders (either here or in
-            # `build_decoders`), so don't apply later
             post_slice = None
     else:
-        in_signal = nengo.builder.connection.slice_signal(model, in_signal, conn.pre_slice)
+        in_signal = nengo.builder.connection.slice_signal(
+            model, in_signal, conn.pre_slice)
 
-    # Build transform
     if conn.solver.weights and not conn.solver.compositional:
-        # special case for non-compositional weight solvers, where
-        # the solver is solving for the full weight matrix. so we don't
-        # need to combine decoders/transform/encoders.
         weighted, weights = model.build(
-            Dense(decoders.shape, init=decoders), in_signal, rng=rng
-        )
+            AIEDense(decoders.shape, init=decoders), in_signal, rng=rng)  # type: ignore
     else:
         weighted, weights = model.build(
-            conn.transform, in_signal, decoders=decoders, encoders=encoders, rng=rng
-        )
+            conn.transform, in_signal, decoders=decoders, encoders=encoders, rng=rng)  # type: ignore
 
     model.sig[conn]["weights"] = weights
 
-    # Build synapse
     if conn.synapse is not None:
         weighted = model.build(conn.synapse, weighted, mode="update")
 
-    # Store the weighted-filtered output in case we want to probe it
-    model.sig[conn]["weighted"] = weighted
+    if isinstance(conn.post_obj, nengo.ensemble.Neurons):
+        gains = nengo.builder.Signal(
+            model.params[conn.post_obj.ensemble].gain[post_slice], name=f"{conn}.gains")
 
-    if isinstance(conn.post_obj, Neurons):
-        # Apply neuron gains (we don't need to do this if we're connecting to
-        # an Ensemble, because the gains are rolled into the encoders)
-        gains = Signal(
-            model.params[conn.post_obj.ensemble].gain[post_slice],
-            name=f"{conn}.gains",
-        )
-
-        if is_integer(post_slice) or isinstance(post_slice, slice):
+        if nengo.utils.numpy.is_integer(post_slice) or isinstance(post_slice, slice):
             sliced_out = model.sig[conn]["out"][post_slice]
         else:
-            # advanced indexing not supported on Signals, so we need to set up an
-            # intermediate signal and use a Copy op to perform the indexing
-            sliced_out = Signal(shape=gains.shape, name=f"{conn}.sliced_out")
-            model.add_op(Reset(sliced_out))
+            sliced_out = nengo.builder.Signal(
+                shape=gains.shape, name=f"{conn}.sliced_out")
+            model.add_op(AIEReset(sliced_out))
             model.add_op(
-                Copy(sliced_out, model.sig[conn]["out"], dst_slice=post_slice, inc=True)
-            )
+                AIECopy(sliced_out, model.sig[conn]["out"], dst_slice=post_slice, inc=True))
 
-        model.add_op(
-            ElementwiseInc(
-                gains, weighted, sliced_out, tag=f"{conn}.gains_elementwiseinc"
-            )
-        )
+        model.add_op(AIEElementwiseInc(gains, weighted, sliced_out,
+                     tag=f"{conn}.gains_elementwiseinc"))
     else:
-        # Copy to the proper slice
         model.add_op(
-            Copy(
-                weighted,
-                model.sig[conn]["out"],
-                dst_slice=post_slice,
-                inc=True,
-                tag=f"{conn}",
-            )
-        )
+            AIECopy(weighted, model.sig[conn]["out"], dst_slice=post_slice, inc=True, tag=f"{conn}"))
 
-    # Build learning rules
     if conn.learning_rule is not None:
-        # TODO: provide a general way for transforms to expose learnable params
-        if not isinstance(conn.transform, (Dense, NoTransform)):
+        if not isinstance(conn.transform, (nengo.Dense, nengo.transforms.NoTransform)):
             raise NotImplementedError(
                 f"Learning on connections with {type(conn.transform).__name__} "
                 "transforms is not supported"
             )
 
         rule = conn.learning_rule
-        rule = [rule] if not is_iterable(rule) else rule
+        rule = [rule] if not nengo.utils.numpy.is_iterable(rule) else rule
         targets = []
+
         for r in rule.values() if isinstance(rule, dict) else rule:
             model.build(r)
             targets.append(r.modifies)
@@ -153,16 +127,13 @@ def build_connection(model, conn):
         if "encoders" in targets:
             encoder_sig = model.sig[conn.post_obj]["encoders"]
             encoder_sig.readonly = False
-        if "decoders" in targets or "weights" in targets:
+
+        if "decoders" in "targets" or "weights" in targets:
             if weights.ndim < 2:
-                raise BuildError(
+                raise nengo.exceptions.BuildError(
                     "'transform' must be a 2-dimensional array for learning"
                 )
             model.sig[conn]["weights"].readonly = False
 
-    model.params[conn] = nengo.builder.connection.BuiltConnection(
-        eval_points=eval_points,
-        solver_info=solver_info,
-        transform=conn.transform,
-        weights=getattr(weights, "initial_value", None),
-    )
+        model.params[conn] = nengo.builder.connection.BuiltConnection(eval_points=eval_points, solver_info=solver_info,
+                                                                      transform=conn.transform, weights=getattr(weights, "initial_value", None))
