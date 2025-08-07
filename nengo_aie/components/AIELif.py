a35933fb2e58fe4f813ac64186f47bb16bc319e4
Andrea Oggioni (aupcloud) <24810621+etabeta1@users.noreply.github.com>
2025-08-07 13:16:58 +0000

    merge: actually solve merge conflicts

--- nengo_aie/components/AIELif.py
+++ nengo_aie/components/AIELif.py
@@ -57,7 +57,6 @@ class AIELif(nengo.neurons.LIF):
 
         output_v = self.output_bo.read(self.size * ITEMSIZE * 3, 0).view(ITEMTYPE)
 
-<<<<<<< HEAD
         try:
             output[...] = output_v[0:output.size]
             voltage[...] = output_v[self.size:self.size+voltage.size]
@@ -66,13 +65,7 @@ class AIELif(nengo.neurons.LIF):
             np.savetxt("in.txt", copy)
             np.savetxt("out.txt", output_v)
             raise
-=======
-        output[...] = output_v[0:output.size]
-        voltage[...] = output_v[self.size:self.size + voltage.size]
-        refractory_time[...] = output_v[2 *
-                                        self.size:2 * self.size + refractory_time.size]
 
->>>>>>> refs/remotes/origin/main
         # output[...] = self.output_bo.read(self.size * ITEMSIZE, 0).view(ITEMTYPE)[:output.size]
         # voltage[...] = self.output_bo.read(self.size * ITEMSIZE, self.size * ITEMSIZE).view(ITEMTYPE)[:voltage.size]
         # refractory_time[...] = self.output_bo.read(self.size * ITEMSIZE, 2 * self.size * ITEMSIZE).view(ITEMTYPE)[:refractory_time.size]
